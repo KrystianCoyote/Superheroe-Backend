bf70773664e1153e4699653717f8e26a5d8002b8
KrystianCoyote <krystianyahirg@gmail.com>
2026-06-04 09:29:23 -0700

    Actualización de los estilos

--- superheroe-frontend/src/app/app.ts
+++ superheroe-frontend/src/app/app.ts
@@ -8,15 +8,13 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  // Cambiado a public para que el HTML pueda leer sus métodos sin restricciones
   public authService = inject(AuthService);
   private router = inject(Router);
 
   logout() {
-    // Usamos el logout centralizado de tu servicio que limpia el token correctamente
     this.authService.logout();
     this.router.navigate(['/login']);
   }
